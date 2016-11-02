a48fc660226f57710e49e99085a12a713efa50f1
Alfredo Arellano <alfredo.arellano@hpe.com>
2016-11-02 17:07:28 +0100

    add example

---
+++ examples/sample/api/schemas/Foo.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+   attributes: { // Mongoose schema
+      name : String,
+      password : String,
+      tenantid : String,
+      roles: [String],
+      firstName: String,
+      lastName: String,
+      email: String
+   },
+
+   statics: {  // Scheme statics
+
+   },
+
+   methods: {  // Scheme methods
+
+   },
+
+   plugins: {
+
+   }
+}
---
+++ examples/sample/config/mongoose.js
@@ -0,0 +1,24 @@
+/*module.exports.mongoose = {
+   connection: {
+      uri: "localhost",
+      options: {
+         database: "utumiki",
+         port: 27017,
+         user: 'utumiki',
+         pass: 'utumiki'
+      }
+   }
+}
+*/
+module.exports.mongoose = {
+   connection: {
+      uri: "uri",
+      options: {
+         database: "database",
+         port: 27017,
+         user: 'user',
+         pass: 'pass'
+      }
+   }
+}
+
